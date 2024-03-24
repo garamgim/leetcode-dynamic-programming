a20d84c418cc909de1fc1cee87a61dbb4021347d
garamgim <garamgim@gmail.com>
2024-03-24 19:50:12 +0900

    Add solution 1137

--- FibonacciStyle/NthTribonacciNumber.java
+++ FibonacciStyle/NthTribonacciNumber.java
@@ -1,2 +1,24 @@
-package FibonacciStyle;public class NthTribonacciNumber {
+package FibonacciStyle;
+// LeetCode 1137 N-th Tribonacci Number
+// https://leetcode.com/problems/n-th-tribonacci-number/description/?envType=study-plan-v2&envId=dynamic-programming
+
+class NthTribonacciNumber {
+    public int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 1;
+
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int d = a + b + c;
+
+        for (int i = 3; i < n; i++) {
+            a = b;
+            b = c;
+            c = d;
+            d = a + b + c;
+        }
+        return d;
+    }
 }
