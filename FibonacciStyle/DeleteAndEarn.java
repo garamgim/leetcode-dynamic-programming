bd2857a2e863046841c83e3ce7aadf8d03788452
garamgim <garamgim@gmail.com>
2024-03-25 12:17:04 +0900

    Modify solution 740

--- FibonacciStyle/DeleteAndEarn.java
+++ FibonacciStyle/DeleteAndEarn.java
@@ -15,19 +15,10 @@ class DeleteAndEarn {
         for (int i = 0; i < nums.length; i++) {
             counting[nums[i]]++;
         }
-        int ans = Integer.MIN_VALUE;
-        for (int i = 0; i < N + 1; i++) {
-            if (i > 4) {
-                int p = dp[i-2] + (i*counting[i]);
-                int q = Math.max(p, dp[i-3] + (i*counting[i]));
-                dp[i] = Math.max(q, dp[i-4] + (i*counting[i]));
-            } else if (i > 3) {
-                dp[i] = Math.max(dp[i-2] + (i*counting[i]), dp[i-3] + (i*counting[i]));
-            } else if (i > 2) {
-                dp[i] = dp[i - 2] + (i * counting[i]);
-            } else {
-                dp[i] = i * counting[i];
-            }
+        dp[1] = counting[1];
+        int ans = dp[1];
+        for (int i = 2; i < N + 1; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + i*counting[i]);
             ans = Math.max(ans, dp[i]);
         }
         System.out.println(Arrays.toString(dp));
