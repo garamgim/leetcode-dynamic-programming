d8548afac636582361941dc1b7d1f4a4642f5f17
garamgim <garamgim@gmail.com>
2024-03-25 12:07:16 +0900

    Modify solution 509

--- FibonacciStyle/FibonacciNumber.java
+++ FibonacciStyle/FibonacciNumber.java
@@ -3,18 +3,19 @@
 
 package FibonacciStyle;
 
-public class FibonacciNumber {
-    public int climbStairs(int n) {
+class FibonacciNumber {
+    public int fib(int n) {
+        if (n == 0) return 0;
         if (n == 1) return 1;
-        if (n == 2) return 2;
-        else {
-            int[] dp = new int[n];
-            dp[0] = 1;
-            dp[1] = 2;
-            for (int i = 2; i < n; i++) {
-                dp[i] = dp[i - 2] + dp[i - 1];
-            }
-            return dp[n - 1];
+
+        int a = 0;
+        int b = 1;
+        int c = a + b;
+        for (int i = 2; i < n; i++) {
+            a = b;
+            b = c;
+            c = a + b;
         }
+        return c;
     }
 }
